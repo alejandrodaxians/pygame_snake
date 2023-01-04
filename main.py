5551678e819469ee6087af73d7c63480467adea7
LeoJPG <lyl102938475647382910@gmail.com>
2023-01-04 16:49:45 +0100

    Added logic to play music.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from pygame import mixer
+
 from gameplay import Gameplay
 
 snake_game = Gameplay()
 
 
 def play_snake():
+    mixer.init()
+    mixer.music.load("FallenDown.mp3")
+    mixer.music.set_volume(0.5)
+    mixer.music.play()
     while True:
         snake_game.snake_movement()
         snake_game.fruit_interaction()
@@ -16,3 +22,4 @@ def play_snake():
 
 if __name__ == "__main__":
     play_snake()
+
